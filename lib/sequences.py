34d5e3ce4f3e48106589c343a56b8835dfbfa09f
Aspen Wilson <aspen.schell@gmail.com>
2023-05-14 07:30:32 -0700

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonancci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,144,233,377,610,987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229]
+    if (length == 0):
+        print([])
+    else:
+        print(fibonancci[0:length])
